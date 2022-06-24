68b6020556351110c97c8e40a36f013d86f055dc
AlinaMendez <alimendez.1998@gmail.com>
2022-06-24 19:53:59 +0000

    Actualizacion

--- src/app.py
+++ src/app.py
@@ -1,73 +1,54 @@
+import requests 
 import pandas as pd
-import requests
-import sqlite3
 from bs4 import BeautifulSoup
+import sqlite3
+import matplotlib.pyplot as plt
 
-#read from source
 url = "https://www.macrotrends.net/stocks/charts/TSLA/tesla/revenue"
 html_data = requests.get(url).text
+#print(html_data)
+result = BeautifulSoup(html_data,"html.parser")
+#print(result)
+tables = result.find_all("table")
+#print(tables)
 
-#parse the html data, html code
-soup = BeautifulSoup(html_data,"html.parser")
+for index,table in enumerate(tables):
+    if ("Tesla Quarterly Revenue" in str(table)):
+        my_index = index
+#print(my_index)
 
-#search the table tag
-tablas=soup.findAll("table")
+df = pd.DataFrame(columns=['Date', 'Revenue'])
 
-#search the table index of "Tesla Quarterly Revenue"
-index_find=0
-for i, tabla in(enumerate(tablas)):
-    #enumarate asigna i=indice tabla=contenido
-    if ("Tesla Quarterly Revenue" in str(tabla)):
-        index_find=i
+for row in tables[my_index].tbody.find_all('tr'):
+    col = row.find_all('td')
+    if (col != []):
+        Fecha = col[0].text
+        Ingreso = col[1].text.replace("$", "").replace(",", "")
+        df = df.append({"Date":Fecha, "Revenue":Ingreso}, ignore_index=True)
 
-#print(tablas[index_find])
+df = df[df['Revenue'] != ""] # elimino los datos nulos
+print(df)
+print(type(df))
 
+records = df.to_records(index=False)
+result = list(records)
+#print(result)
 
-#go inside table and save the values
-#df=pd.DataFrame(columns=["date","revenue"])
+connection = sqlite3.connect('Tesla.db')
+cursor = connection.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS revenue
+             (Date, Revenue)''')
 
-dataValues = []
-#df = pd.DataFrame()
-for rowT in tablas[index_find].tbody.find_all("tr"):
-    #for each tr (row), take the td (column) value
-    colT= rowT.find_all("td")
-    if len(colT)>0:
-        dateVal=colT[0].text
-    revenueVal=colT[1].text.replace("$","").replace(",","")
-    #print(dateVal," ",revenueVal)
-    dataValues.append({"date":dateVal,"revenue":revenueVal})
+cursor.executemany('INSERT INTO revenue VALUES (?,?)', result)
+connection.commit()
+for x in cursor.execute('SELECT * FROM revenue'):
+    print(x)
 
-#df = df.append({"date":dateVal,"revenue":revenueVal},ignore_index=True)
-#deprecated method.
+df['Date'] = pd.to_datetime(df['Date'])
+df['Revenue'] = df['Revenue'].astype('int64')
+plt.figure()
+plt.scatter(x=df['Date'], y=df['Revenue'])
+plt.title("Tesla Quarterly Revenue")
+plt.ylabel("Revenue (Millions of US $)")
+plt.savefig("Revenue.jpg")
 
-#load data to the dataframe
-df = pd.DataFrame.from_records(dataValues)
-#print(df.tail(10))
-
-#not null in exploration, if find use dropna
-#quito vacios
-df=df[df["revenue"] != ""]
-
-#change type
-df['date'] = df['date'].astype('datetime64')
-df['revenue'] = df['revenue'].astype('int64')
-
-###### STEP DATABASE #########
-
-#create the file where save the database if not exists yet
-conn = sqlite3.connect('Tesla.db') 
-c = conn.cursor()
-
-#load the dataframe to the database
-df.to_sql("Tesla",conn,if_exists='replace',index=False)
-
-conn.commit()
-
-#read from database the data
-sel_df = pd.read_sql("select * from Tesla",conn)
-print(sel_df)
-
-conn.close()
-
-#can show a exponencial function
-df.plot(kind="scatter",x="date",y="revenue")
